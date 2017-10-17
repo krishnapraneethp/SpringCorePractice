b1bde379af73978a1709b0a0a345c541b57e4ecd
krishnapraneethp <krishnapraneethp@Krishna-Thinkpad.krishnapraneethp>
2017-10-17 12:50:58 +0530

    Spring Tutorial 06 - Using Constructor Injection

--- SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/shapes/Triangle.java
+++ SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/shapes/Triangle.java
@@ -1,17 +1,45 @@
 package practice.spring.core.shapes;
 
 public class Triangle {
+
+	private int height;
+	private int width;
+	private String author;
 	private String type;
-	
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
 	public String getType() {
 		return type;
 	}
-	
-	public void setType(String type) {
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Triangle(String type) {
 		this.type = type;
 	}
 
+	public Triangle(int height) {
+		this.height = height;
+	}
+
+	public Triangle(int height, int width, String type, String author) {
+		this.type = type;
+		this.height = height;
+		this.author = author;
+		this.width = width;
+	}
+
 	public void draw() {
-		System.out.println(getType()+" Triangle drawn");
+		System.out.println(this.getType() + " Triangle drawn by " + this.getAuthor() + " with height "
+				+ this.getHeight() + " and width " + this.getWidth());
 	}
 }
