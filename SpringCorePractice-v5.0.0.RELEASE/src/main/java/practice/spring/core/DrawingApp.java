ee3ca3e3687c91b086d45ac10f55b041023ceffb
krishnapraneethp <krishnapraneethp@Krishna-Thinkpad.krishnapraneethp>
2017-10-17 11:55:51 +0530

    Spring Tutorial 05 - ApplicationContext and Property Initialization

--- SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/DrawingApp.java
+++ SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/DrawingApp.java
@@ -1,17 +1,15 @@
 package practice.spring.core;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import practice.spring.core.shapes.Triangle;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-//		Triangle triangle = new Triangle();
-		BeanFactory  beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 
