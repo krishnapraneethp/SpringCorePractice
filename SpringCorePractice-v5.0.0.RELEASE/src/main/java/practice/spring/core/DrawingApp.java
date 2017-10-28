0ebe8bd0236c65ca1c470f8243a125b44e03f4c5
krishnapraneethp <krishnapraneethp@Krishna-ThinkPad-Edge-E431>
2017-10-28 18:40:42 +0530

    Spring Tutorial 17 - Coding To Interfaces

--- SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/DrawingApp.java
+++ SpringCorePractice-v5.0.0.RELEASE/src/main/java/practice/spring/core/DrawingApp.java
@@ -3,15 +3,21 @@ package practice.spring.core;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import practice.spring.core.shapes.Rectangle;
+import practice.spring.core.utilities.shapes.Shape;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-			AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext applicationContext= null;
+		try {
+			applicationContext = new ClassPathXmlApplicationContext("spring.xml");
 			applicationContext.registerShutdownHook();
-			Rectangle rectangle = (Rectangle) applicationContext.getBean("rectangle");
-			System.out.println("A triangle was drawn: "+rectangle.draw());
+			Shape rectangle = (Shape) applicationContext.getBean("triangle");
+			System.out.println(rectangle.draw());
+		} finally {
 			applicationContext.close();
+		}
+			
+			
 	}
 }
